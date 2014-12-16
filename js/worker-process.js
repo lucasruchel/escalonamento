1094e9a4e1c0d95aa65a0241538e5fa5ebef7295
Lucas Ruchel <lukasruchel@gmail.com>
2014-12-16 10:29:01 -0200

    todos os algoritmos implementados

--- js/worker-process.js
+++ js/worker-process.js
@@ -57,36 +57,80 @@ function roundRobin(){
 					//Atribui 0 a tempo de burst do processo.
 					processList_rr[i].burstTime = 0;
 					//Adiciona na tabela os dados do processo
-					addRow(tableProcessData,cont,processList[i].insertedPosition,processList[i].priority,processList[i].burstTime,averageWait);
+					addRow(tableProcessData,cont,processList_rr[i].insertedPosition,processList_rr[i].priority,processList_rr[i].burstTime,averageWait);
 					// "Remove elemento" adicionando na verdade um elemento nulo na posição do processo que foi concluido
 					processList_rr[i] = null;
 				}
 			}
 		}
-
+	//Caso nenhum processo esteja ativo (processAlive==false) então saia do laço
 	}while(processAlive);
+
+	delete processList_rr;
 }
 //Necessario ordenar por prioridade
 function priorityScheduler(){
-	var tableProcessData = document.querySelector("#srtnData");
+	var tableProcessData = document.querySelector("#prioritySchedulerData");
+	var processList_ps = processList.slice(0);
+	var averageWait = 0;
+	//Contador de Execucoes
+	var cont=0;
+	//Atributo para verificar se existe algum objeto ativo na lista rr
+	var processAlive;
+	//limpa o campo que armazena as informações
+	tableProcessData.innerHTML = "";
+
+	processList_ps.sort(sortByPriority);
+
+	for(var i=0 ; i < processList_ps.length ; i++){
+		addRow(tableProcessData,i,processList_ps[i].insertedPosition,processList_ps[i].priority,processList_ps[i].burstTime,averageWait);
+		averageWait += processList_ps[i].burstTime;
+	}
+}
 
 
+function sortByBurst(a,b){
+	if(a.burstTime>b.burstTime)
+	 	return (a.burstTime-b.burstTime);
+
+	 return;
+}
+function sortByPriority(a,b){
+	if(a.priority>b.priority)
+		return (a.priority-b.priority);
+	return;
 }
 
 function shortestJobFirst(){
+	var tableProcessData = document.querySelector("#sjfData");
+	var processList_sjf = processList.slice(0);
+	var averageWait = 0;
+	//Contador de Execucoes
+	var cont=0;
+	//Atributo para verificar se existe algum objeto ativo na lista rr
+	var processAlive;
+	//limpa o campo que armazena as informações
+	tableProcessData.innerHTML = "";
 
+	processList_sjf.sort(sortByBurst);
+
+	for(var i=0 ; i < processList_sjf.length ; i++){
+		addRow(tableProcessData,i,processList_sjf[i].insertedPosition,processList_sjf[i].priority,processList_sjf[i].burstTime,averageWait);
+		averageWait += processList_sjf[i].burstTime;
+	}
 }
 function firstComeFirstServed(){
 
 	var tableProcessData = document.querySelector("#fcfsData");
 	var averageWait=0;
+	var processList_fcfs = processList.slice(0);
 	//reset entry
 	tableProcessData.innerHTML="";
 
 	
-	for(var i=0 ; i < processList.length ; i++){
-		addRow(tableProcessData,i,processList[i].insertedPosition,processList[i].priority,processList[i].burstTime,averageWait);
-		averageWait += processList[i].burstTime;
+	for(var i=0 ; i < processList_fcfs.length ; i++){
+		addRow(tableProcessData,i,processList_fcfs[i].insertedPosition,processList_fcfs[i].priority,processList_fcfs[i].burstTime,averageWait);
+		averageWait += processList_fcfs[i].burstTime;
 	}
 }
 function addRow(element,cont,id,priority,burstTime,averageWait){
