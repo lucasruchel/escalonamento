934fe70df6bb708482f11c14a3d9279a3980d5be
Lucas Ruchel <lukasruchel@gmail.com>
2014-12-17 00:30:31 -0200

    tempo medio implementado, menos round robin

--- js/event-handler.js
+++ js/event-handler.js
@@ -2,18 +2,20 @@
 var btnQuantum = document.querySelector("#add-quantum");
 var btnAddProcess = document.querySelector("#add-process");
 var btnExecute = document.querySelector('#execute');
+	quantum = 0;
 	
 	//Show Process properties on a table
 	tableData = document.querySelector("#processData");
 
 	//counter of process entries
-	cont = 0;
+	count = 0;
 
 btnQuantum.addEventListener('click',function(){
 	var checkQuantum = document.querySelector('#quantum-time').value;
 
 	if (!isNaN(checkQuantum)){
 		quantum	= parseInt(checkQuantum);
+		alert("Quantum adicionado com sucesso!!");
 	}
 	 else{
 	 	document.querySelector(".cpu-time-field .alert").classList.add("alert-error");
@@ -27,7 +29,7 @@ btnAddProcess.addEventListener('click',function(){
 	if  ((!isNaN(checkBurst)) && (!isNaN(checkPrority))) {
 		priority = parseInt(checkPrority);
 		burstTime = parseInt(checkBurst);
-		var process = new Process(++cont,priority,burstTime);
+		var process = new Process(++count,priority,burstTime);
 		processList.push(process);
 		addTableData(process);
 	}
@@ -42,6 +44,14 @@ function addTableData(processData){
 btnExecute.addEventListener('click', function(){
 	var operations = document.querySelectorAll(".algorith-option:checked");
 	
+	if (quantum==0){
+		alert("defina um valor para o quantum");
+		return;
+	}else if(count<3){
+		alert("Defina ao menos 3 elementos");
+		return;
+	}
+
 	for (var i=0; i < operations.length ; i++){
 		if (operations[i].name == "priorityScheduler")
 			priorityScheduler();
--- js/worker-process.js
+++ js/worker-process.js
@@ -8,6 +8,8 @@ Process = function(insertedPosition,priority,burstTime){
 	this.burstTime = burstTime;
 	//total executed time
 	this.executedTime = 0;
+
+	this.lastExecutedTime = 0;
 }
 
 //This variable will storage the process 
@@ -23,6 +25,13 @@ Array.prototype.clone = function(){
 	};
 	return temp;
 };
+Array.prototype.haveAlive = function(){
+	for (var i=0; i < this.length; i++) {
+		if(this[i].burstTime>0)
+			return true;
+	};
+	return false;
+}
 
 //Necessario executar o quantum de CPU utilizando uma fila FIFO
 function roundRobin(){
@@ -39,6 +48,8 @@ function roundRobin(){
 	//limpa o campo que armazena as informações
 	tableProcessData.innerHTML = "";
 	//Inicio o loop sem testar condicao alguma
+	var lastIndex = 0;
+	var indexTemp = 0;
 	do{
 		//Atribui inicialmente falso aos processos ativos
 		processAlive=false;
@@ -48,39 +59,50 @@ function roundRobin(){
 			if(processList_rr[i].burstTime > 0){
 				//seta o atributo de processo ativo até que o loop do-while tenha sido concluido
 				processAlive=true;
-				//incrementa o contador para uma execucao
+				
 				cont++;
+				
 				//Testa se o tempo de burst vai ser maior que o do quantum, para o valor da subtracao não seja negativa
 				if(processList_rr[i].burstTime-quantum > 0){
 					//Subtrai o valor do quantum do tempo de burst do processo
 					processList_rr[i].burstTime -= quantum;
-
-					processList_rr[i].executedTime = quantum;
+					//processList_rr[i].executedTime += quantum;
 
 					//Adiciona na tabela os dados do processo
-					addRow(tableProcessData,cont,processList[i].insertedPosition,processList[i].priority,processList[i].burstTime,averageWait);
+					addRow(tableProcessData,cont,processList_rr[i].insertedPosition,processList_rr[i].priority,processList_rr[i].burstTime,averageWait);
 
-					averageWait += quantum;
+					
+					
+					if(lastIndex != processList_rr[i].insertedPosition){
+						averageWait += quantum;
+						lastIndex = processList_rr[i].insertedPosition
+					}
 				//Caso o tempo de burst restante do processo seja menor ou igual ao do quantum	
 				}else{
 					//Atribui 0 a tempo de burst do processo.
 					var temp = processList_rr[i].burstTime;
-					processList_rr[i].executedTime = processList_rr[i].burstTime;
+					processList_rr[i].executedTime += processList_rr[i].burstTime;
 					processList_rr[i].burstTime = 0;
-
+					processList_rr[i].lastExecutedTime = averageWait
 					//Adiciona na tabela os dados do processo
 					addRow(tableProcessData,cont,processList_rr[i].insertedPosition,processList_rr[i].priority,processList_rr[i].burstTime,averageWait);
 					//Se o tempo do quantum for maior do que é necessario para ser executado entao o tempo de espera é o mesmo que o de burst
-					averageWait += temp; 
-					// "Remove elemento" adicionando na verdade um elemento nulo na posição do processo que foi concluido
-					//processList_rr[i] = null;
+
+					
+
+					if(lastIndex == processList_rr[i].insertedPosition){
+						
+						
+					}else{
+						lastIndex = processList_rr[i].insertedPosition;
+						averageWait += temp; 
+					}
+					
 				}
 			}
 		}
 	//Caso nenhum processo esteja ativo (processAlive==false) então saia do laço
 	}while(processAlive);
-
-	delete processList_rr;
 }
 //Necessario ordenar por prioridade
 function priorityScheduler(){
@@ -98,27 +120,18 @@ function priorityScheduler(){
 
 	for(var i=0 ; i < processList_ps.length ; i++){
 		addRow(tableProcessData,i,processList_ps[i].insertedPosition,processList_ps[i].priority,processList_ps[i].burstTime,averageWait);
-		averageWait += processList_ps[i].burstTime;
+		if ( i<processList_ps.length-1 ) {
+			averageWait += processList_ps[i].burstTime;	
+		};
 	}
+	showAverage(tableProcessData,averageWait,processList_ps.length);
 }
-
-
-function sortByBurst(a,b){
-	if(a.burstTime>b.burstTime)
-	 	return (a.burstTime-b.burstTime);
-
-	 return;
-}
-function sortByPriority(a,b){
-	if(a.priority>b.priority)
-		return (a.priority-b.priority);
-	return;
-}
-
 function shortestJobFirst(){
 	var tableProcessData = document.querySelector("#sjfData");
 	var processList_sjf = processList.clone();
 	var averageWait = 0;
+	var lastIndex = false;
+	var temp=0;
 	//Contador de Execucoes
 	var cont=0;
 	//Atributo para verificar se existe algum objeto ativo na lista rr
@@ -130,8 +143,11 @@ function shortestJobFirst(){
 
 	for(var i=0 ; i < processList_sjf.length ; i++){
 		addRow(tableProcessData,i,processList_sjf[i].insertedPosition,processList_sjf[i].priority,processList_sjf[i].burstTime,averageWait);
-		averageWait += processList_sjf[i].burstTime;
+		if ( i<processList_sjf.length-1 ) {
+			averageWait += processList_sjf[i].burstTime;	
+		};
 	}
+	showAverage(tableProcessData,averageWait,processList_sjf.length);
 }
 function firstComeFirstServed(){
 
@@ -144,9 +160,32 @@ function firstComeFirstServed(){
 	
 	for(var i=0 ; i < processList_fcfs.length ; i++){
 		addRow(tableProcessData,i,processList_fcfs[i].insertedPosition,processList_fcfs[i].priority,processList_fcfs[i].burstTime,averageWait);
-		averageWait += processList_fcfs[i].burstTime;
+		if ( i<processList_fcfs.length-1 ) {
+			averageWait += processList_fcfs[i].burstTime;	
+		};
+		
 	}
+	showAverage(tableProcessData,averageWait,processList_fcfs.length);
 }
+//--------------- Output Functions -------------------------------------
 function addRow(element,cont,id,priority,burstTime,averageWait){
 		element.innerHTML += "<tr>"+"<td>" + cont +"</td>" +"<td>" + id +"</td>"+"<td>" + priority +"</td>"+"<td>" + burstTime +"</td>"+"<td>" + averageWait +"</td>"+ "</tr>";
 }
+function showAverage(element,maxTime,maxItens){
+	element.innerHTML += "<tr><td colspan='"+element.parentNode.rows[0].cells.length+"'><h3>Tempo médio de Execução: "+maxTime/maxItens+"ms</h3></td></tr>";	
+}
+//----------------------------------------------------------------------
+//--------------- Funções para ordenar elementos -----------------------
+
+function sortByBurst(a,b){
+	if(a.burstTime>b.burstTime)
+	 	return (a.burstTime-b.burstTime);
+
+	 return;
+}
+function sortByPriority(a,b){
+	if(a.priority>b.priority)
+		return (a.priority-b.priority);
+	return;
+}
+//----------------------------------------------------------------------
